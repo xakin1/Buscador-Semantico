12ad4762f8abc054a41912c2461014c72484b987
Xaquín Pérez Bermo <xaquin.perez@imatia.com>
2021-06-08 17:29:02 +0200

    fixed aplly stash

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ComponentsRoutingModule } from './components-routing.module';
-import { SharedModule } from '../shared.module';
-import { InfoComponent } from './info/info.component';
-import { LabelsComponent } from './labels/labels.component';
+
 import { SliderComponent } from './slider/slider.component';
 import { DdComponent } from './dd/dd.component';
 import { MatAutocompleteModule, MatButtonModule, MatChipsModule, MatFormFieldModule, MatIconModule, MatRadioModule, MatSliderModule, MatStepperModule, MatToolbarModule, MatTooltipModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
+import { InfoComponent } from './info/info.component';
+import { LabelsComponent } from './labels/labels.component';
+import { ComponentsRoutingModule } from './components-routing.module';
 
 
 
