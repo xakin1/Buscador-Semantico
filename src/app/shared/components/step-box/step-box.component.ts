bdd7a863a0b35c5097b451c3dc0477b5f96b0037
Xaquín Pérez Bermo <xaquin.perez@imatia.com>
2021-06-25 22:15:29 +0200

    Nueva construcción del árbol
    arreglar condiciones (si las cambias se genera una nueva en vez de actualizar solo el string)
    añadir un true false
    añadir siguiente step

--- src/app/shared/components/step-box/step-box.component.ts
+++ src/app/shared/components/step-box/step-box.component.ts
@@ -3,6 +3,17 @@ import { Component, ComponentFactoryResolver, ComponentRef, EventEmitter, Input,
 import { ColumnComponent } from '../column/column.component';
 import { StepBoxRightComponent } from '../step-box-right/step-box-right.component';
 
+
+export interface Step{
+  id: string
+  name: string,
+  haveNext: boolean,
+  synonym: any[],
+  keywords: any[],
+  dd: any[],
+  conditions: any[]
+}
+
 @Component({
   selector: 'app-step-box',
   templateUrl: './step-box.component.html',
@@ -13,39 +24,21 @@ export class StepBoxComponent implements OnInit {
 
   @Output() edit = new EventEmitter<any>();
   @Input("titleStep") titleStep = "Title of Step"
-  constructor(private resolver: ComponentFactoryResolver) { }
+  constructor() { }
   public unique_key: number = 0;
   public haveNext = false;
   open: boolean = true;
   edited: boolean = false;
   index : number = 0;
-
-  @ViewChild("viewContainer", { read: ViewContainerRef, static : false }) VCR: ViewContainerRef
-  componentsReferences = Array<ComponentRef<any>>()
+  columns: Step[][] = []
 
   ngOnInit() {
   }
 
-  editStep(){
-    this.edit.emit({column: 0, index:this.unique_key});
+  editStep(row, column){
+    this.edit.emit({row: row, column: column});
   }
 
-  createColumn(key){
-
-    let componentFactory = this.resolver.resolveComponentFactory(ColumnComponent);
-
-    let childComponentRef  = this.VCR.createComponent(componentFactory);
-    let childComponent     = childComponentRef.instance;
-    childComponent.unique_key = key+1;
-    this.componentsReferences.push(childComponentRef);
-    childComponent.edit.subscribe(($event) => {
-      this.edit.emit({column: childComponent.unique_key-1, index: $event.index, position: $event.position})
-    });
-  }
-
-  public createStep(index,key) {
-    this.componentsReferences[index].instance.steps.push({id: key, name: 'Title of Step', haveNext: false});
-  }
 
 
 }
--- src/app/tree-of-steps/tree/tree.component.ts
+++ src/app/tree-of-steps/tree/tree.component.ts
@@ -2,19 +2,7 @@ import { ComponentFactoryResolver, ComponentRef, ViewChild, ViewContainerRef } f
 import { Component, OnInit } from '@angular/core';
 import { StepBoxComponent } from 'src/app/shared/components/step-box/step-box.component';
 import 'leader-line';
-import { StepBoxRightComponent } from 'src/app/shared/components/step-box-right/step-box-right.component';
-import { ColumnComponent } from 'src/app/shared/components/column/column.component';
 declare let LeaderLine: any;
-
-
-export interface Step {
-  title: string,
-  synonym: any,
-  keywords: any,
-  dd: any,
-  conditions: any
-}
-
 
 @Component({
   selector: 'app-tree',
@@ -29,9 +17,8 @@ export class TreeComponent implements OnInit {
   condicionaltrue  = "condicional true"
   condicional  = "condicional"
   child_unique_key: number = -1;
-  index: number = 0;
+  row: number = 0;
   column: number = 0;
-  position: number =0;
 
   infoKeywords: string = "Hacer click en una etiqueta para desplegar el diccionario de datos"
   info : string = "Regla que, junto las palabras claves, ayudarán a identificar el producto";
@@ -45,12 +32,9 @@ export class TreeComponent implements OnInit {
   placeholderSynonym: string = "Nuevo sinónimo...";
   widthSynonym: number = 100;
   conditionOfselectedStep = [];
+  step: any;
 
   @ViewChild("viewContainer", { read: ViewContainerRef, static : false }) VCR: ViewContainerRef
-  componentsReferences = Array<ComponentRef<any>>()
-
-  steps: Step[] = [];
-
 
   constructor(private resolver: ComponentFactoryResolver) { }
 
@@ -59,30 +43,20 @@ export class TreeComponent implements OnInit {
 
   saveNameStep() {
     if((<HTMLInputElement>document.getElementById("Title")) != undefined){
-      if(this.index == 0){
-        this.componentsReferences[this.index].instance.titleStep =  (<HTMLInputElement>document.getElementById("Title")).value;
-      }
-      else{
-        console.log("column "+ this.column + " index "+ this.index)
-        console.log(this.componentsReferences[0].instance)
-        this.componentsReferences[0].instance.componentsReferences[this.column].instance.steps[this.position].name =  (<HTMLInputElement>document.getElementById("Title")).value;
-      }
+        this.step[this.column][this.row].name = (<HTMLInputElement>document.getElementById("Title")).value;
     }
   }
 
   saveCondition(i) {
     if((<HTMLInputElement>document.getElementById("Condicion " + i)) != undefined){
         this.createNextStep((<HTMLInputElement>document.getElementById("Condicion " + i)).value);
-        if( this.steps[this.index].conditions[i] != undefined)  this.steps[this.index].conditions[i] = (<HTMLInputElement>document.getElementById("Condicion " + i)).value;
-        else this.steps[this.index].conditions.push((<HTMLInputElement>document.getElementById("Condicion " + i)).value);
-
+        if( this.step[this.column][this.row].conditions[i] != undefined )  this.step[this.column][this.row].conditions[i] = (<HTMLInputElement>document.getElementById("Condicion " + i)).value;
+        else this.step[this.column][this.row].conditions.push((<HTMLInputElement>document.getElementById("Condicion " + i)).value);
     }
-
   }
 
   newCondition(){
-    console.log(this.steps)
-    this.steps[this.index].conditions.push('');
+    this.step[this.column][this.row].conditions.push('');
   }
 
   toggleSidebar(){
@@ -91,147 +65,78 @@ export class TreeComponent implements OnInit {
   }
 
   createStep(){
-
     let componentFactory = this.resolver.resolveComponentFactory(StepBoxComponent);
 
     let childComponentRef  = this.VCR.createComponent(componentFactory);
     let childComponent     = childComponentRef.instance;
-    childComponent.unique_key = ++this.child_unique_key;
 
     childComponent.edit.subscribe(($event) => {
       this.edit = true;
       this.open = true;
-      this.index = $event.index;
+      this.row = $event.row;
       this.column = $event.column;
-      this.position = $event.position
 
-      if(this.steps[this.index] != undefined) this.conditionOfselectedStep = this.steps[this.index].conditions;
+      //TODO: REVISAR
+      if(this.step[this.column][this.row] != undefined) this.conditionOfselectedStep = this.step[this.column][this.row].conditions;
       else this.conditionOfselectedStep = [];
-
-      if((<HTMLInputElement>document.getElementById("Title")) != undefined){
-        if(this.index == 0){
-          (<HTMLInputElement>document.getElementById("Title")).value = this.componentsReferences[this.index].instance.titleStep;
-
-        }
-        else{
-          (<HTMLInputElement>document.getElementById("Title")).value = this.componentsReferences[0].instance.componentsReferences[this.column].instance.steps[this.position].name
-        }
-      }
+      if(<HTMLInputElement>document.getElementById("Title") != undefined)
+        (<HTMLInputElement>document.getElementById("Title")).value = this.step[this.column][this.row].name
     });
 
-    this.steps.push({
-      title:  '',
-      synonym: [],
-      keywords: [], dd: [],
-      conditions: []})
-
-    // add reference for newly created component
     this.toggleSidebar();
 
-    this.componentsReferences.push(childComponentRef)
-
+    childComponent.columns.push([{id: this.column+" "+this.row , name: "Title of Step", haveNext: false, conditions: [],dd: [],keywords: [], synonym: [] }])
+    this.step = childComponent.columns;
   }
 
-
-  createColumnN(key){
-    let componentFactory = this.resolver.resolveComponentFactory(ColumnComponent);
-
-    let childComponentRef  = this.VCR.createComponent(componentFactory);
-    let childComponent     = childComponentRef.instance;
-    childComponent.unique_key = key+1;
-    this.componentsReferences.push(childComponentRef);
-    this.componentsReferences[childComponent.unique_key].instance.steps.push({id: key, name: 'Title of Step', haveNext: false});
-
-  }
-
-  public createStepN(index,key) {
-
+  drawLine(startElement, endElement, label){
+    if(label != ''){
+        new LeaderLine({
+        start: startElement ,
+        end: endElement,
+        endLabel: label
+      })
+    }
+    else{
+        new LeaderLine({
+        start: startElement,
+        end: endElement
+        });
+    }
   }
 
   createColumn(label){
-     this.createColumnN(this.child_unique_key)
+    this.step[this.column][this.row].haveNext = true
 
-      setTimeout(()=>{
-        new LeaderLine({
-        start: (<HTMLInputElement>document.getElementById(this.index+"")),
-        end: (<HTMLInputElement>document.getElementById(""+ this.child_unique_key)),
-        endLabel: label
-        });
-      });
+    this.step.push([{id: this.step.length +" 0", name: 'Title of Step', haveNext: false, conditions: [],dd: [],keywords: [], synonym: []}]);
 
-      this.toggleSidebar();
-  }
-
-  createStepInColumn(vector,label){
-    vector.createStep(this.index,++this.child_unique_key+'');
     setTimeout(()=>{
-      new LeaderLine({
-      start: (<HTMLInputElement>document.getElementById(this.index+"")),
-      end: (<HTMLInputElement>document.getElementById(""+this.child_unique_key)),
-      endLabel: label
-      });
-    });
+      let startElement = (<HTMLInputElement>document.getElementById(this.column +" "+this.row));
+      let endElement = (<HTMLInputElement>document.getElementById(this.step.length-1  +" 0"))
+      this.drawLine(startElement,endElement,label );
+    })
+
     this.toggleSidebar();
   }
 
-  // createColumn(label){
-  //     this.componentsReferences[0].instance.createColumn(this.child_unique_key)
+  createStepInColumn(label) {
 
+    this.step[this.column+1].push({id:( this.step.length -1) +" "+this.step[this.column+1].length, name: 'Title of Step', haveNext: false, conditions: [],dd: [],keywords: [], synonym: []})
 
-  //     this.componentsReferences[0].instance.createStep(this.index,++this.child_unique_key+'')
+    setTimeout(()=>{
+      console.log("start "+ this.column +" "+this.row)
+      let startElement = (<HTMLInputElement>document.getElementById(this.column+" "+this.row));
+      console.log("end "+ (this.step.length-1)+" "+ (this.step[this.column+1].length - 1))
+      let endElement = (<HTMLInputElement>document.getElementById(this.step.length-1+" "+ (this.step[this.column+1].length-1)))
+      this.drawLine(startElement,endElement,label);
+    })
 
-  //     setTimeout(()=>{
-  //       new LeaderLine({
-  //       start: (<HTMLInputElement>document.getElementById(this.index+"")),
-  //       end: (<HTMLInputElement>document.getElementById(""+ this.child_unique_key)),
-  //       endLabel: label
-  //       });
-  //     });
-
-  //     this.toggleSidebar();
-  // }
-
-  // createStepInColumn(vector,label){
-  //   vector.createStep(this.index,++this.child_unique_key+'');
-  //   setTimeout(()=>{
-  //     new LeaderLine({
-  //     start: (<HTMLInputElement>document.getElementById(this.index+"")),
-  //     end: (<HTMLInputElement>document.getElementById(""+this.child_unique_key)),
-  //     endLabel: label
-  //     });
-  //   });
-  //   this.toggleSidebar();
-  // }
-
-  createNextStep(label){
-
-    if(this.index >= 1){
-      if(this.componentsReferences[0].instance.componentsReferences[this.column].instance.steps[this.position].haveNext == false){
-        this.componentsReferences[0].instance.componentsReferences[this.column].instance.steps[this.position].haveNext = true
-        this.createColumn(label)
-      }
-      else {
-        this.createStepInColumn(this.componentsReferences[0].instance,label)
-      }
-
-    }
-    else{
-      if(this.componentsReferences[0].instance.haveNext == false){
-        this.componentsReferences[0].instance.haveNext = true
-        this.createColumn(label)
-      }
-      else {
-        this.createStepInColumn(this.componentsReferences[0].instance,label)
-      }
-    }
-
-    this.steps.push({
-      title:  '',
-      synonym: [],
-      keywords: [], dd: [],
-      conditions: []})
+    this.toggleSidebar();
   }
 
-
+  createNextStep(label){
+    if(this.step[this.column+1] == undefined) this.createColumn(label)
+    else this.createStepInColumn(label)
+  }
 
 }
